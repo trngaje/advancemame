a023bf2f77b0631b3f51fbabbda058cdce6933c5
trngaje <bilbobaggins@naver.com>
2021-06-08 13:23:42 +0000

    update for advmame hangul

--- advance/osd/menu.c
+++ advance/osd/menu.c
@@ -57,7 +57,7 @@ static int video_mode_menu(struct advance_video_context* context, struct advance
 
 	advance_ui_menu_init(&menu);
 
-	advance_ui_menu_title_insert(&menu, "Video Mode");
+	advance_ui_menu_title_insert(&menu, ui_getstring(UI_video_mode)/*"Video Mode"*/);
 
 	auto_index = advance_ui_menu_text_insert(&menu, "Auto");
 
@@ -149,10 +149,10 @@ static int video_pipeline_menu(struct advance_video_context* context, struct adv
 		++i;
 	}
 
-	advance_ui_menu_title_insert(&menu, "Pipeline Blit Time");
+	advance_ui_menu_title_insert(&menu, ui_getstring(UI_pipeline_blit_time)/*"Pipeline Blit Time"*/);
 
 	if (context->state.pipeline_measure_flag) {
-		advance_ui_menu_text_insert(&menu, "Time measure not completed");
+		advance_ui_menu_text_insert(&menu, ui_getstring(UI_time_measure_not_completed)/*"Time measure not completed"*/);
 	} else {
 		double timing = adv_measure_median(0.00001, 0.5, context->state.update_timing_map, PIPELINE_MEASURE_MAX);
 		snprintf(buffer, sizeof(buffer), "Last render %.2f (ms)", timing * 1000);
@@ -258,7 +258,7 @@ int osd2_video_menu(int selected, unsigned input)
 
 	advance_ui_menu_init(&menu);
 
-	advance_ui_menu_title_insert(&menu, "Video Menu");
+	advance_ui_menu_title_insert(&menu, ui_getstring(UI_video_menu)/*"Video Menu"*/);
 
 	snprintf(buffer, sizeof(buffer), "%dx%dx%d %.1f/%.1f/%.1f",
 		video_size_x(),
@@ -270,16 +270,16 @@ int osd2_video_menu(int selected, unsigned input)
 	);
 	advance_ui_menu_title_insert(&menu, buffer);
 
-	resolution_index = advance_ui_menu_option_insert(&menu, "Mode...", video_context->config.resolution_buffer);
+	resolution_index = advance_ui_menu_option_insert(&menu, ui_getstring(UI_mode)/*"Mode..."*/, video_context->config.resolution_buffer);
 
 	if (!video_context->state.game_vector_flag) {
 		if (strcmp(video_context->config.resolution_buffer, "auto") == 0) {
 			switch (mode_current_magnify(video_context)) {
 			default:
-			case 1: text = "Magnify [1]"; break;
-			case 2: text = "Magnify [2]"; break;
-			case 3: text = "Magnify [3]"; break;
-			case 4: text = "Magnify [4]"; break;
+			case 1: text = ui_getstring(UI_magnify_1)/*"Magnify [1]"*/; break;
+			case 2: text = ui_getstring(UI_magnify_2)/*"Magnify [2]"*/; break;
+			case 3: text = ui_getstring(UI_magnify_3)/*"Magnify [3]"*/; break;
+			case 4: text = ui_getstring(UI_magnify_4)/*"Magnify [4]"*/; break;
 			}
 			switch (video_context->config.magnify_factor) {
 			default:
@@ -294,13 +294,13 @@ int osd2_video_menu(int selected, unsigned input)
 			magnify_index = -1;
 		}
 
-		advance_ui_menu_title_insert(&menu, "Options");
+		advance_ui_menu_title_insert(&menu, ui_getstring(UI_options)/*"Options"*/);
 
 		switch (mode_current_stretch(video_context)) {
-		case STRETCH_NONE: text = "Resize [no]"; break;
-		case STRETCH_INTEGER_XY: text = "Resize [integer]"; break;
-		case STRETCH_INTEGER_X_FRACTIONAL_Y: text = "Resize [mixed]"; break;
-		case STRETCH_FRACTIONAL_XY: text = "Resize [fractional]"; break;
+		case STRETCH_NONE: text = ui_getstring(UI_resize_no)/*"Resize [no]"*/; break;
+		case STRETCH_INTEGER_XY: text = ui_getstring(UI_resize_integer)/*"Resize [integer]"*/; break;
+		case STRETCH_INTEGER_X_FRACTIONAL_Y: text = ui_getstring(UI_resize_mixed)/*"Resize [mixed]"*/; break;
+		case STRETCH_FRACTIONAL_XY: text = ui_getstring(UI_resize_fractional)/*"Resize [fractional]"*/; break;
 		}
 		switch (video_context->config.stretch) {
 		case STRETCH_NONE: option = "no"; break;
@@ -319,25 +319,25 @@ int osd2_video_menu(int selected, unsigned input)
 
 	switch (video_index()) {
 	case MODE_FLAGS_INDEX_PALETTE8:
-		text = "Color [palette8]";
+		text = ui_getstring(UI_color_palette8)/*"Color [palette8]"*/;
 		break;
 	case MODE_FLAGS_INDEX_BGR8:
-		text = "Color [bgr8]";
+		text = ui_getstring(UI_color_bgr8)/*"Color [bgr8]"*/;
 		break;
 	case MODE_FLAGS_INDEX_BGR15:
-		text = "Color [bgr15]";
+		text = ui_getstring(UI_color_bgr15)/*"Color [bgr15]"*/;
 		break;
 	case MODE_FLAGS_INDEX_BGR16:
-		text = "Color [bgr16]";
+		text = ui_getstring(UI_color_bgr16)/*"Color [bgr16]"*/;
 		break;
 	case MODE_FLAGS_INDEX_BGR32:
-		text = "Color [bgr32]";
+		text = ui_getstring(UI_color_bgr32)/*"Color [bgr32]"*/;
 		break;
 	case MODE_FLAGS_INDEX_YUY2:
-		text = "Color [yuy2]";
+		text = ui_getstring(UI_color_yuy2)/*"Color [yuy2]"*/;
 		break;
 	default:
-		text = "Color [unknown]";
+		text = ui_getstring(UI_color_unknown)/*"Color [unknown]"*/;
 		break;
 	}
 	switch (video_context->config.index) {
@@ -369,14 +369,14 @@ int osd2_video_menu(int selected, unsigned input)
 	index_index = advance_ui_menu_option_insert(&menu, text, option);
 
 	switch (video_context->state.combine) {
-	case COMBINE_NONE: text = "Resize Effect [no]"; break;
-	case COMBINE_MAXMIN: text = "Resize Effect [max]"; break;
-	case COMBINE_MEAN: text = "Resize Effect [mean]"; break;
-	case COMBINE_FILTER: text = "Resize Effect [filter]"; break;
-	case COMBINE_SCALEX: text = "Resize Effect [scalex]"; break;
-	case COMBINE_SCALEK: text = "Resize Effect [scalek]"; break;
-	case COMBINE_HQ: text = "Resize Effect [hq]"; break;
-	case COMBINE_XBR: text = "Resize Effect [xbr]"; break;
+	case COMBINE_NONE: text = ui_getstring(UI_resize_effect_no)/*"Resize Effect [no]"*/; break;
+	case COMBINE_MAXMIN: text = ui_getstring(UI_resize_effect_max)/*"Resize Effect [max]"*/; break;
+	case COMBINE_MEAN: text = ui_getstring(UI_resize_effect_mean)/*"Resize Effect [mean]"*/; break;
+	case COMBINE_FILTER: text = ui_getstring(UI_resize_effect_filter)/*"Resize Effect [filter]"*/; break;
+	case COMBINE_SCALEX: text = ui_getstring(UI_resize_effect_scalex)/*"Resize Effect [scalex]"*/; break;
+	case COMBINE_SCALEK: text = ui_getstring(UI_resize_effect_scalek)/*"Resize Effect [scalek]"*/; break;
+	case COMBINE_HQ: text = ui_getstring(UI_resize_effect_hq)/*"Resize Effect [hq]"*/; break;
+	case COMBINE_XBR: text = ui_getstring(UI_resize_effect_xbr)/*"Resize Effect [xbr]"*/; break;
 	}
 	switch (video_context->config.combine) {
 	case COMBINE_AUTO: option = "auto"; break;
@@ -392,17 +392,17 @@ int osd2_video_menu(int selected, unsigned input)
 	combine_index = advance_ui_menu_option_insert(&menu, text, option);
 
 	switch (video_context->state.rgb_effect) {
-	case EFFECT_NONE: text = "Rgb Effect [no]"; break;
-	case EFFECT_RGB_TRIAD3PIX: text = "Rgb Effect [triad3dot]"; break;
-	case EFFECT_RGB_TRIAD6PIX: text = "Rgb Effect [triad6dot]"; break;
-	case EFFECT_RGB_TRIAD16PIX: text = "Rgb Effect [triad16dot]"; break;
-	case EFFECT_RGB_TRIADSTRONG3PIX: text = "Rgb Effect [triadstrong3dot]"; break;
-	case EFFECT_RGB_TRIADSTRONG6PIX: text = "Rgb Effect [triadstrong6dot]"; break;
-	case EFFECT_RGB_TRIADSTRONG16PIX: text = "Rgb Effect [triadstrong16dot]"; break;
-	case EFFECT_RGB_SCANDOUBLEHORZ: text = "Rgb Effect [scan2horz]"; break;
-	case EFFECT_RGB_SCANTRIPLEHORZ: text = "Rgb Effect [scan3horz]"; break;
-	case EFFECT_RGB_SCANDOUBLEVERT: text = "Rgb Effect [scan2vert]"; break;
-	case EFFECT_RGB_SCANTRIPLEVERT: text = "Rgb Effect [scan3vert]"; break;
+	case EFFECT_NONE: text = ui_getstring(UI_rgb_effect_no)/*"Rgb Effect [no]"*/; break;
+	case EFFECT_RGB_TRIAD3PIX: text = ui_getstring(UI_rgb_effect_triad3dot)/*"Rgb Effect [triad3dot]"*/; break;
+	case EFFECT_RGB_TRIAD6PIX: text = ui_getstring(UI_rgb_effect_triad6dot)/*Rgb Effect [triad6dot]"*/; break;
+	case EFFECT_RGB_TRIAD16PIX: text = ui_getstring(UI_rgb_effect_triad16dot)/*"Rgb Effect [triad16dot]"*/; break;
+	case EFFECT_RGB_TRIADSTRONG3PIX: text = ui_getstring(UI_rgb_effect_triadstrong3dot)/*"Rgb Effect [triadstrong3dot]"*/; break;
+	case EFFECT_RGB_TRIADSTRONG6PIX: text = ui_getstring(UI_rgb_effect_triadstrong6dot)/*"Rgb Effect [triadstrong6dot]"*/; break;
+	case EFFECT_RGB_TRIADSTRONG16PIX: text = ui_getstring(UI_rgb_effect_triadstrong16dot)/*"Rgb Effect [triadstrong16dot]"*/; break;
+	case EFFECT_RGB_SCANDOUBLEHORZ: text = ui_getstring(UI_rgb_effect_scan2horz)/*"Rgb Effect [scan2horz]"*/; break;
+	case EFFECT_RGB_SCANTRIPLEHORZ: text = ui_getstring(UI_rgb_effect_scan3horz)/*"Rgb Effect [scan3horz]"*/; break;
+	case EFFECT_RGB_SCANDOUBLEVERT: text = ui_getstring(UI_rgb_effect_scan2vert)/*"Rgb Effect [scan2vert]"*/; break;
+	case EFFECT_RGB_SCANTRIPLEVERT: text = ui_getstring(UI_rgb_effect_scan3vert)/*"Rgb Effect [scan3vert]"*/; break;
 	}
 	switch (video_context->config.rgb_effect) {
 	case EFFECT_NONE: option = "no"; break;
@@ -446,21 +446,21 @@ int osd2_video_menu(int selected, unsigned input)
 	smp_index = -1;
 #endif
 
-	pipeline_index = advance_ui_menu_text_insert(&menu, "Details...");
+	pipeline_index = advance_ui_menu_text_insert(&menu, ui_getstring(UI_details)/*"Details..."*/);
 
 	if (global_context->state.is_config_writable) {
-		save_game_index = advance_ui_menu_text_insert(&menu, "Save for this game");
+		save_game_index = advance_ui_menu_text_insert(&menu, ui_getstring(UI_save_for_this_game)/*"Save for this game"*/);
 
 		if (!video_context->state.game_vector_flag) {
-			save_resolutionclock_index = advance_ui_menu_text_insert(&menu, "Save for this game size/freq");
+			save_resolutionclock_index = advance_ui_menu_text_insert(&menu, ui_getstring(UI_save_for_this_game_size_freq)/*"Save for this game size/freq"*/);
 		} else {
 			save_resolutionclock_index = -1;
 		}
 
 		if (video_context->state.game_vector_flag)
-			text = "Save for all vector games";
+			text = ui_getstring(UI_save_for_all_vector_games)/*"Save for all vector games"*/;
 		else
-			text = "Save for this game size";
+			text = ui_getstring(UI_save_for_this_game_size)/*"Save for this game size"*/;
 		save_resolution_index = advance_ui_menu_text_insert(&menu, text);
 	} else {
 		save_game_index = -1;
@@ -683,7 +683,7 @@ static int audio_pipeline_menu(struct advance_sound_context* sound_context, stru
 
 	advance_ui_menu_init(&menu);
 
-	advance_ui_menu_title_insert(&menu, "Audio Pipeline");
+	advance_ui_menu_title_insert(&menu, ui_getstring(UI_audio_pipeline)/*"Audio Pipeline"*/);
 
 	if (sound_context->config.normalize_flag) {
 		snprintf(buffer, sizeof(buffer), "(%d) normalize", i);
@@ -818,12 +818,12 @@ int osd2_audio_menu(int selected, unsigned input)
 
 	advance_ui_menu_init(&menu);
 
-	advance_ui_menu_title_insert(&menu, "Audio Menu");
+	advance_ui_menu_title_insert(&menu, ui_getstring(UI_audio_menu)/*"Audio Menu"*/);
 
 	switch (sound_context->state.output_mode) {
-	case SOUND_MODE_MONO: text = "Mode [mono]"; break;
-	case SOUND_MODE_STEREO: text = "Mode [stereo]"; break;
-	case SOUND_MODE_SURROUND: text = "Mode [surround]"; break;
+	case SOUND_MODE_MONO: text = ui_getstring(UI_mode_mono)/*"Mode [mono]"*/; break;
+	case SOUND_MODE_STEREO: text = ui_getstring(UI_mode_stereo)/*"Mode [stereo]"*/; break;
+	case SOUND_MODE_SURROUND: text = ui_getstring(UI_mode_surround)/*"Mode [surround]"*/; break;
 	}
 	switch (sound_context->config.mode) {
 	case SOUND_MODE_AUTO: option = "auto"; break;
@@ -837,7 +837,7 @@ int osd2_audio_menu(int selected, unsigned input)
 		snprintf(buffer, sizeof(buffer), "%d", sound_context->config.attenuation);
 	else
 		sncpy(buffer, sizeof(buffer), "mute");
-	volume_index = advance_ui_menu_option_insert(&menu, "Attenuation (dB)", buffer);
+	volume_index = advance_ui_menu_option_insert(&menu, ui_getstring(UI_attenuation)/*"Attenuation (dB)"*/, buffer);
 
 	advance_ui_menu_title_insert(&menu, "Normalize");
 
@@ -845,35 +845,35 @@ int osd2_audio_menu(int selected, unsigned input)
 		option = "yes";
 	else
 		option = "no";
-	normalize_index = advance_ui_menu_option_insert(&menu, "Auto normalize", option);
+	normalize_index = advance_ui_menu_option_insert(&menu, ui_getstring(UI_auto_normalize)/*"Auto normalize"*/, option);
 
 	snprintf(buffer, sizeof(buffer), "%d", sound_context->config.adjust);
-	adjust_index = advance_ui_menu_option_insert(&menu, "Amplifier (dB)", buffer);
+	adjust_index = advance_ui_menu_option_insert(&menu, ui_getstring(UI_amplifier)/*"Amplifier (dB)"*/, buffer);
 
-	advance_ui_menu_title_insert(&menu, "Equalizer");
+	advance_ui_menu_title_insert(&menu, ui_getstring(UI_equalizer)/*"Equalizer"*/);
 
 	if (sound_context->config.equalizer_low > -40)
 		snprintf(buffer, sizeof(buffer), "%d", sound_context->config.equalizer_low);
 	else
 		sncpy(buffer, sizeof(buffer), "mute");
-	eql_index = advance_ui_menu_option_insert(&menu, "Low Freq (dB)", buffer);
+	eql_index = advance_ui_menu_option_insert(&menu, ui_getstring(UI_low_freq)/*"Low Freq (dB)"*/, buffer);
 
 	if (sound_context->config.equalizer_mid > -40)
 		snprintf(buffer, sizeof(buffer), "%d", sound_context->config.equalizer_mid);
 	else
 		sncpy(buffer, sizeof(buffer), "mute");
-	eqm_index = advance_ui_menu_option_insert(&menu, "Mid Freq (dB)", buffer);
+	eqm_index = advance_ui_menu_option_insert(&menu, ui_getstring(UI_mid_freq)/*"Mid Freq (dB)"*/, buffer);
 
 	if (sound_context->config.equalizer_high > -40)
 		snprintf(buffer, sizeof(buffer), "%d", sound_context->config.equalizer_high);
 	else
 		sncpy(buffer, sizeof(buffer), "mute");
-	eqh_index = advance_ui_menu_option_insert(&menu, "High Freq (dB)", buffer);
+	eqh_index = advance_ui_menu_option_insert(&menu, ui_getstring(UI_high_freq)/*"High Freq (dB)"*/, buffer);
 
-	pipeline_index = advance_ui_menu_text_insert(&menu, "Details...");
+	pipeline_index = advance_ui_menu_text_insert(&menu, ui_getstring(UI_details)/*"Details..."*/);
 
 	if (global_context->state.is_config_writable) {
-		save_game_index = advance_ui_menu_text_insert(&menu, "Save for this game");
+		save_game_index = advance_ui_menu_text_insert(&menu, ui_getstring(UI_save_for_this_game)/*"Save for this game"*/);
 	} else {
 		save_game_index = -1;
 	}
